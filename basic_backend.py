fceccb1e63c846515192fdff906b63f5f5fb3265
Thi Hieu <thihieuluu@gmail.com>
2021-02-22 10:49:53 +0100

    Test basic_backend with change player.Player by ClassType

--- basic_backend.py
+++ basic_backend.py
@@ -66,16 +66,16 @@ def delete_item(key, value):
 
 
 if __name__ == '__main__':
-    p1 = player.Player(234, "Lucrèce", "Besnard", "12/02/2021", "M", 1070)
-    p2 = player.Player(172, "Alexis", "Marchant", "18/01/2021", "M", 670)
-    p3 = player.Player(896, "Gaël", "Charpentier", "12/02/2021", "M", 470)
-    p4 = player.Player(742, "Marius", "Bouhier", "09/01/2021", "M", 1250)
-    p5 = player.Player(666, "Jean-François", "Duchemin", "02/01/2021", "M", 570)
-    p6 = player.Player(957, "Frédéric", "Colbert", "06/01/2021", "M", 870)
-    p7 = player.Player(482, "Gaëtan", "Trintignant", "27/01/2021", "M", 770)
-    p8 = player.Player(976, "Abraham", "Adnet", "12/01/2021", "M", 970)
-    p9 = player.Player(348, "Nicolas", "Desmarais", "05/01/2021", "M", 1170)
-    p10 = player.Player(523, " Claude", "Brassard", "03/01/2021", "M", 1470)
+    p1 = ClassType(234, "Lucrèce", "Besnard", "12/02/2021", "M", 1070)
+    p2 = ClassType(172, "Alexis", "Marchant", "18/01/2021", "M", 670)
+    p3 = ClassType(896, "Gaël", "Charpentier", "12/02/2021", "M", 470)
+    p4 = ClassType(742, "Marius", "Bouhier", "09/01/2021", "M", 1250)
+    p5 = ClassType(666, "Jean-François", "Duchemin", "02/01/2021", "M", 570)
+    p6 = ClassType(957, "Frédéric", "Colbert", "06/01/2021", "M", 870)
+    p7 = ClassType(482, "Gaëtan", "Trintignant", "27/01/2021", "M", 770)
+    p8 = ClassType(976, "Abraham", "Adnet", "12/01/2021", "M", 970)
+    p9 = ClassType(348, "Nicolas", "Desmarais", "05/01/2021", "M", 1170)
+    p10 = ClassType(523, " Claude", "Brassard", "03/01/2021", "M", 1470)
 
     attributes_values = {"fide_id": 234,
                          "first_name": "a",

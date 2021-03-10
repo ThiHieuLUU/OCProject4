fd6788b6e1b6d0e9cb30d4c31bfff1babcc785ad
Thi Hieu <thihieuluu@gmail.com>
2021-03-10 12:49:30 +0100

    Delete some files which are moved into chessmodel package

--- mvc_exceptions.py
+++
@@ -1,9 +0,0 @@
-"""There are some exceptions which will be met in the process of checking players, rounds, matches, etc."""
-
-
-class RoundIndexError(Exception):
-    pass
-
-
-class EmptyListError(Exception):
-    pass
